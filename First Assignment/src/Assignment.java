996e38d8b42b8b737a2953a9e71948a5434400eb
Fazal ur Rehman <fazal.rehman.sqa.com>
2018-03-05 19:17:20 +0500

    Facebook Sign up page automated

---
+++ First Assignment/src/Assignment.java
@@ -0,0 +1,63 @@
+
+public class Assignment {
+
+	public static void main(String[] args) {
+		
+		//Code to out put simple statments :) 
+		/*	System.out.println("++++++");
+		System.out.println("@@@@@@");
+		System.out.println("******");
+		System.out.println("######");
+		
+		*/
+		
+		//Code to declare variables etc 
+		
+		/*int a; 
+		double b,c;
+			a=10;
+			b=10.32;
+			c=20.904;
+		
+			System.out.println(a);
+
+			System.out.println(b);
+
+			System.out.println(c);
+			*/
+		
+		//Declaration of Boolean
+		
+		/*boolean a; 
+		a = true; 
+		a=false;
+		System.out.println(a);
+*/
+		
+		
+          //  int value = 18;
+
+            //System.out.println("The value of the integer variable is : " + value);
+
+   
+		
+		
+		String [] array = {"Fazal", "Ali", "Ahmed", "Muzamil"};
+		
+		for(int i = 0; i<=array.length; i ++)
+		{
+			System.out.println("Values are" + array[i]);
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
